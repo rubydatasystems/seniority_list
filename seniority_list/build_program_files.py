d7122b7ab0c86e6db03dc47072fde4439772ee7b
bob_davison <b2002@fastmail.net>
2016-08-13 15:32:55 -0400

    change intial_editor_vals "int_mnum" value (0) to a string for editor text dropdown widget

--- seniority_list/build_program_files.py
+++ seniority_list/build_program_files.py
@@ -128,7 +128,7 @@ high = int(.8 * rows)
 
 init_editor_vals = pd.DataFrame([['<<  d', '2', 'ret_mark', 'spcnt', 'log',
                                 False, '==', '1', high, False, True,
-                                low, 100, '>=', 0]],
+                                low, 100, '>=', '0']],
                                 columns=['drop_dir_val', 'drop_eg_val',
                                          'drop_filter', 'drop_msr',
                                          'drop_sq_val', 'fit_val',
