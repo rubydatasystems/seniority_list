d4cf2b36220af39c50d37df4745258bf13dccd89
bob_davison <b2002@fastmail.net>
2016-11-26 13:48:38 -0500

    add filter to clear_dill_files function to keep __init__.py file in dill folder when removing all pickled files when changing case studies...

--- seniority_list/functions.py
+++ seniority_list/functions.py
@@ -3196,7 +3196,7 @@ def clear_dill_files():
     used when changing case study, avoids possibility of file
     from previos calculations being used by new study
     '''
-    filelist = [f for f in os.listdir("dill/")]
+    filelist = [f for f in os.listdir("dill/") if f.endswith('.pkl')]
     for f in filelist:
         os.remove('dill/' + f)
 
